841d49906ed986214a27a94c33580855cf7083a9
Administrator <admin@seemsys.com>
2019-08-06 10:24:53 +0430

    Fixed a bug that caused MYSQL error "Too many connections"

--- src/com.atrosys/util/HibernateUtil.java
+++ src/com.atrosys/util/HibernateUtil.java
@@ -2,31 +2,17 @@ package com.atrosys.util;
 
 import org.hibernate.Query;
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.hibernate.cfg.Configuration;
 
 import java.util.LinkedList;
 import java.util.List;
 
 
 public class HibernateUtil {
-    private static final SessionFactory session_factory;
 
-    static {
-        session_factory = new Configuration().configure().buildSessionFactory();
-    }
 
     public Session getSession() {
-        ThreadLocal thread_session = new ThreadLocal();
-        Session session = (Session) thread_session.get();
-
-        if (session == null ) {
-            session = session_factory.openSession();
-            thread_session.set(session);
-        }
-
-        return session;
+        return SessionUtil.getSession();
     }
 
     public  Object getOneRow(Query query) throws Exception{
