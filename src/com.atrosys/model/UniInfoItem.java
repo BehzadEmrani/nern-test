a28ce55670e3124aa93754023277f56b7a8f3c13
Administrator <admin@seemsys.com>
2019-11-27 13:43:31 +0330

    New model for getting uni-info in POST api

---
+++ src/com.atrosys/model/UniInfoItem.java
@@ -0,0 +1,221 @@
+package com.atrosys.model;
+
+public class UniInfoItem {
+    private Long uniNationalId;
+    private String uniName;
+    private String uniEcoCode;
+    private String uniTopManagerName;
+    private String uniTopManagerPos;
+    private String uniSignatoryName;
+    private String uniSignatoryPos;
+    private String uniSignatoryNationalId;
+    private String State;
+    private String City;
+    private String uniAddress;
+    private String uniPostalCode;
+    private String uniTelNo;
+    private String uniFaxNo;
+    private String uniWebsite;
+    private String uniEmail;
+    private String uniRequestForm;
+    private String agentFname;
+    private String agentLname;
+    private long agentNationalId;
+    private String agentPos;
+    private String agentTeleNo;
+    private String agentMobileNo;
+    private String agentFaxNo;
+
+    public Long getUniNationalId() {
+        return uniNationalId;
+    }
+
+    public void setUniNationalId(Long uniNationalId) {
+        this.uniNationalId = uniNationalId;
+    }
+
+    public String getUniName() {
+        return uniName;
+    }
+
+    public void setUniName(String uniName) {
+        this.uniName = uniName;
+    }
+
+    public String getUniEcoCode() {
+        return uniEcoCode;
+    }
+
+    public void setUniEcoCode(String uniEcoCode) {
+        this.uniEcoCode = uniEcoCode;
+    }
+
+    public String getUniTopManagerName() {
+        return uniTopManagerName;
+    }
+
+    public void setUniTopManagerName(String uniTopManagerName) {
+        this.uniTopManagerName = uniTopManagerName;
+    }
+
+    public String getUniTopManagerPos() {
+        return uniTopManagerPos;
+    }
+
+    public void setUniTopManagerPos(String uniTopManagerPos) {
+        this.uniTopManagerPos = uniTopManagerPos;
+    }
+
+    public String getUniSignatoryName() {
+        return uniSignatoryName;
+    }
+
+    public void setUniSignatoryName(String uniSignatoryName) {
+        this.uniSignatoryName = uniSignatoryName;
+    }
+
+    public String getUniSignatoryPos() {
+        return uniSignatoryPos;
+    }
+
+    public void setUniSignatoryPos(String uniSignatoryPos) {
+        this.uniSignatoryPos = uniSignatoryPos;
+    }
+
+    public String getUniSignatoryNationalId() {
+        return uniSignatoryNationalId;
+    }
+
+    public void setUniSignatoryNationalId(String uniSignatoryNationalId) {
+        this.uniSignatoryNationalId = uniSignatoryNationalId;
+    }
+
+    public String getState() {
+        return State;
+    }
+
+    public void setState(String state) {
+        State = state;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public void setCity(String city) {
+        City = city;
+    }
+
+    public String getUniAddress() {
+        return uniAddress;
+    }
+
+    public void setUniAddress(String uniAddress) {
+        this.uniAddress = uniAddress;
+    }
+
+    public String getUniPostalCode() {
+        return uniPostalCode;
+    }
+
+    public void setUniPostalCode(String uniPostalCode) {
+        this.uniPostalCode = uniPostalCode;
+    }
+
+    public String getUniTelNo() {
+        return uniTelNo;
+    }
+
+    public void setUniTelNo(String uniTelNo) {
+        this.uniTelNo = uniTelNo;
+    }
+
+    public String getUniFaxNo() {
+        return uniFaxNo;
+    }
+
+    public void setUniFaxNo(String uniFaxNo) {
+        this.uniFaxNo = uniFaxNo;
+    }
+
+    public String getUniWebsite() {
+        return uniWebsite;
+    }
+
+    public void setUniWebsite(String uniWebsite) {
+        this.uniWebsite = uniWebsite;
+    }
+
+    public String getUniEmail() {
+        return uniEmail;
+    }
+
+    public void setUniEmail(String uniEmail) {
+        this.uniEmail = uniEmail;
+    }
+
+    public String getUniRequestForm() {
+        return uniRequestForm;
+    }
+
+    public void setUniRequestForm(String uniRequestForm) {
+        this.uniRequestForm = uniRequestForm;
+    }
+
+    public String getAgentFname() {
+        return agentFname;
+    }
+
+    public void setAgentFname(String agentFname) {
+        this.agentFname = agentFname;
+    }
+
+    public String getAgentLname() {
+        return agentLname;
+    }
+
+    public void setAgentLname(String agentLname) {
+        this.agentLname = agentLname;
+    }
+
+    public long getAgentNationalId() {
+        return agentNationalId;
+    }
+
+    public void setAgentNationalId(long agentNationalId) {
+        this.agentNationalId = agentNationalId;
+    }
+
+    public String getAgentPos() {
+        return agentPos;
+    }
+
+    public void setAgentPos(String agentPos) {
+        this.agentPos = agentPos;
+    }
+
+    public String getAgentTeleNo() {
+        return agentTeleNo;
+    }
+
+    public void setAgentTeleNo(String agentTeleNo) {
+        this.agentTeleNo = agentTeleNo;
+    }
+
+    public String getAgentMobileNo() {
+        return agentMobileNo;
+    }
+
+    public void setAgentMobileNo(String agentMobileNo) {
+        this.agentMobileNo = agentMobileNo;
+    }
+
+    public String getAgentFaxNo() {
+        return agentFaxNo;
+    }
+
+    public void setAgentFaxNo(String agentFaxNo) {
+        this.agentFaxNo = agentFaxNo;
+    }
+
+}
